57156c3120042b71e0352c2fb2bb67694ed9dede
OkayKenji <70079754+OkayKenji@users.noreply.github.com>
2020-08-22 22:23:31 -0400

    Fizz Buzz Game Script
    Fizzbuzz is a game where you count up from 0. If the number is a multiple of 3 you say "Fizz" when its a multiple of 5 you say "Buzz". When its both a multiple of both 3 and 5 you say "FizzBuzz". 
    This file is written in JavaScript.

---
+++ fizzbuzzgame.js
@@ -0,0 +1,36 @@
+//FizzBuzz is a game where you count up from 0.
+//However when you reach a number that is a mutiple of 3 you say Fizz
+//                                          mutiplee of 5 you say Buzz
+//                                                     both you say FizzBuzz
+//This is inspired by the video by Tom Scott 
+//See his video here:https://www.youtube.com/watch?v=QPZ0pIK_wsc
+function fizzbuzz(  min,  max) {
+    for (var i=min; i<=max ; i++) {
+        if (fizz(i)||buzz(i)) {
+            var str = ""; 
+            if(fizz(i)) {
+                str+="Fizz";
+            }
+            if(buzz(i)) {
+                str+="Buzz";
+            }
+            console.log(str); 
+        }
+        else 
+            console.log(i); 
+    }
+}
+
+function fizz(x) {
+     if (x%3==0)
+        return true;
+     else 
+        return false;
+}
+
+function buzz( y) {
+    if (y%5==0)
+        return true;
+     else 
+        return false;
+}
